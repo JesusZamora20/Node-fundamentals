272aaa0f88f7b0b9498e0edf9cd900b0e4403ed2
Jesus Zamora <jbermudezamora87@gmail.com>
2022-12-14 17:54:19 -0500

    child process

--- Modules/childProcess.js
+++ Modules/childProcess.js
@@ -1,13 +1,27 @@
 const {exec, spawn} = require('child_process');
 // const exec = require('child_process').exec  <= the same as up
 
-exec('node Modules/console.js',(err, stdout, sterr) => {
-    if(err){
-        console.error(err);
-        return false;
-    }
+// exec('node Modules/console',(err, stdout, sterr) => {
+//     if(err){
+//         console.error(err);
+//         return false;
+//     }
 
-    console.log(stdout)
-}) 
+//     console.log(stdout)
+// }) 
 
-let process = spawn('dir')
+let process = spawn('ls',['-al']);
+
+console.log(process.pid);
+console.log(process.connected);
+
+process.stdout.on('data', (dato)=>{
+    console.log('process dead?')
+    console.log(process.killed)
+    console.log(dato.toString())
+});
+
+process.on('exit', () =>{
+    console.log('process finished')
+    console.log(process.killed);
+})
